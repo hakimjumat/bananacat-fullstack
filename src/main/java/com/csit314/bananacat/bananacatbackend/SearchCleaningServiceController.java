50f66cbc0afcb61cfb6f05119765cd2240422e37
Ppanda13 <128679559+Ppanda13@users.noreply.github.com>
2025-05-08 21:31:18 +0800

    Add files via upload
    view number of page view, view shortlist number and view history not done yet

---
+++ src/main/java/com/csit314/bananacat/bananacatbackend/SearchCleaningServiceController.java
@@ -0,0 +1,22 @@
+package com.csit314.bananacat.bananacatbackend;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+public class SearchCleaningServiceController {
+    private final CleaningServiceRepository CSRepository;
+
+    public SearchCleaningServiceController(CleaningServiceRepository CSRepository) {
+        this.CSRepository = CSRepository;
+    }
+
+    @PostMapping("path")
+    public ResponseEntity<?> SearchCleaningService(@RequestBody CleaningServiceEntity CSEntity) {
+        return CSEntity.SearchCleaningService(CSRepository);
+    }
+    
+}
