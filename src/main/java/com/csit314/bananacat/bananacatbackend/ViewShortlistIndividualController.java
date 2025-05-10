8610b814a21354bc7482e2b9aa6662e7cf268966
Ppanda13 <128679559+Ppanda13@users.noreply.github.com>
2025-05-10 19:40:15 +0800

    Add files via upload

---
+++ src/main/java/com/csit314/bananacat/bananacatbackend/ViewShortlistIndividualController.java
@@ -0,0 +1,16 @@
+package com.csit314.bananacat.bananacatbackend;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+public class ViewShortlistIndividualController {
+    @PostMapping("path")
+    public ResponseEntity<?> ViewShortlistIndividual(@RequestBody shortlistEntity slEntity) {
+        return slEntity.ViewShortlistIndividual();
+    }
+    
+}
