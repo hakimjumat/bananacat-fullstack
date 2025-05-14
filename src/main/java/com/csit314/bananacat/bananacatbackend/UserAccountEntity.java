ccbe5b2fb6469320d690801387b83e0aa339de87
Ppanda13 <128679559+Ppanda13@users.noreply.github.com>
2025-05-14 15:45:01 +0800

    Update UserAccountEntity.java

--- src/main/java/com/csit314/bananacat/bananacatbackend/UserAccountEntity.java
+++ src/main/java/com/csit314/bananacat/bananacatbackend/UserAccountEntity.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.servlet.http.HttpSession;
 import jakarta.transaction.Transactional;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -23,7 +22,7 @@ public class UserAccountEntity {
     private String lastname;
     private Integer phonenumber;
     private String address;
-    private Integer NumberofPageView; //for User story #15, rmb to update db make it default 0, cannot be null after account creation cause of nullpointerexception
+    private Integer NumberofPageView; //for User story #15, rmb to update db make it default 0, cannot be null after account creation cause of nullpointerexception shouldnt be displayed except for cleaners
     private Integer ratings; //1 to 5, manual insert through psql, for display only
     
     public String getEmail() {
