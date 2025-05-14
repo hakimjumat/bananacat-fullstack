906bc7f7bd26d3e6bb0e56afe45e58b9c020a013
Ppanda13 <128679559+Ppanda13@users.noreply.github.com>
2025-05-14 15:37:53 +0800

    Delete src/main/java/com/csit314/bananacat/bananacatbackend/WeeklyReportRepository.java

--- src/main/java/com/csit314/bananacat/bananacatbackend/WeeklyReportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.csit314.bananacat.bananacatbackend;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WeeklyReportRepository extends JpaRepository<WeeklyReportEntity, Long>{
-    Optional<WeeklyReportEntity> findByDate(String date);
-}
