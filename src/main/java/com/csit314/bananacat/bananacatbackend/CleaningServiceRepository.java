901a1215ebf30362529f3e6e990c31acf7188408
Ppanda13 <128679559+Ppanda13@users.noreply.github.com>
2025-05-15 15:04:18 +0800

    Update CleaningServiceRepository.java

--- src/main/java/com/csit314/bananacat/bananacatbackend/CleaningServiceRepository.java
+++ src/main/java/com/csit314/bananacat/bananacatbackend/CleaningServiceRepository.java
@@ -2,8 +2,10 @@ package com.csit314.bananacat.bananacatbackend;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+import java.util.List;
 
 public interface CleaningServiceRepository extends JpaRepository<CleaningServiceEntity, Long>{
 
     Optional<CleaningServiceEntity> findByEmailandName(String email, String name);
+    List<CleaningServiceEntity> findByEmail(String email);
 }
